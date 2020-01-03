5db341670bc2c9236de6f43b3732e2299129fdf0
Azek <sudoazek@gmail.com>
2020-01-03 17:13:58 +0900

    Create floating_point_data_type.kt

---
+++ Very basics/Kotlin Data Types/floating_point_data_type.kt
@@ -0,0 +1,15 @@
+fun main(args: Array<String>) {
+var myFloat = 54F  //add suffix L for long integer
+
+    println("My float value: $myFloat")
+
+    var f1: Float = Float.MIN_VALUE
+    var f2: Float = Float.MAX_VALUE
+    println("Smallest Float value: $f1")
+    println("Largest Float value: $f2")
+
+    var d1: Double = Double.MIN_VALUE
+    var d2: Double = Double.MAX_VALUE
+    println("Smallest Double value: $d1")
+    println("Largest Double value: $d2")
+}
