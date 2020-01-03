229dc9efad44eee056079411edb3059265f5db5c
Azek <sudoazek@gmail.com>
2020-01-03 17:05:40 +0900

    Create integer_data_types.kt

---
+++ Very basics/Kotlin Data Types/integer_data_types.kt
@@ -0,0 +1,29 @@
+fun main(args: Array<String>) {
+var myInt = 35
+    //add suffix L for long integer
+    var myLong = 23L
+
+    println("My integer $myInt")
+    println("My long integer $myLong")
+
+    var b1: Byte = Byte.MIN_VALUE
+    var b2: Byte = Byte.MAX_VALUE
+    println("Smallest byte integer value: $b1")
+    println("Largest byte integer value: $b2")
+
+    var s1: Short = Short.MIN_VALUE
+    var s2: Short = Short.MAX_VALUE
+    println("Smallest short integer value: $s1")
+    println("Largest short integer value: $s2")
+
+    var i1: Int = Int.MIN_VALUE
+    var i2: Int = Int.MAX_VALUE
+    println("Smallest integer value: $i1")
+    println("Largest integer value: $i2")
+
+    var l1: Long = Long.MIN_VALUE
+    var l2: Long = Long.MAX_VALUE
+    println("Smallest long integer value: $l1")
+    println("Largest long integer value: $l2")
+
+}
