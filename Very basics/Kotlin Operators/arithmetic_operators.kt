b0f3abd14a4ec0b80ddf7765e9abc0cdb24d94d6
Azek <sudoazek@gmail.com>
2020-01-03 17:51:50 +0900

    Create arithmetic_operators.kt

---
+++ Very basics/Kotlin Operators/arithmetic_operators.kt
@@ -0,0 +1,14 @@
+fun main(args: Array<String>) {
+    var a = 20
+    var b = 4
+    println("a + b = " + (a + b))
+    println("a + b = " + (a.plus(b)))
+    println("a - b = " + (a - b))
+    println("a - b = " + (a.minus(b)))
+    println("a * b = " + (a * b))
+    println("a * b = " + (a.times(b)))
+    println("a / b = " + (a / b))
+    println("a / b = " + (a.div(b)))
+    println("a % b = " + (a % b))
+    println("a % b = " + (a.rem(b)))
+}
