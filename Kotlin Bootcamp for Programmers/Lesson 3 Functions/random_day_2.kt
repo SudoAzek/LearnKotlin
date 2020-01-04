a0052f938d8f13c3c67184be4bb5bef2d10fb629
Azek <sudoazek@gmail.com>
2020-01-04 19:28:31 +0900

    Create random_day_2.kt

---
+++ Kotlin Bootcamp for Programmers/Lesson 3 Functions/random_day_2.kt
@@ -0,0 +1,30 @@
+import java.util.*
+
+fun main(args: Array<String>) {
+    println("Hello, ${args[0]}")
+    feedTheFish()
+}
+
+fun feedTheFish() {
+    val day = randomDay()
+    val food = fishFood(day)
+    println("Today is $day and the fish eat $food")
+}
+
+fun randomDay(): String {
+    val week = listOf("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday")
+    return week[Random().nextInt(7)]
+}
+
+fun fishFood(day: String): String {
+    var food = "fasting"
+
+    return when(day) {
+        "Monday" -> "flakes"
+        "Wednesday" -> "redworms"
+        "Thursday" -> "granules"
+        "Friday" -> "mosquitoes"
+        "Sunday" -> "plankton"
+        else -> "fasting"
+    }
+}
